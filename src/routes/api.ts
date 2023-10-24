8552c1c3f08059c6365fd9a6d859066757f66a14
rkv <dontgofun@gmail.com>
2023-10-24 23:44:37 +0300

    Add swagger ui
    NOTE: The declaration file is just a mock. Unfortunately the Swagger ui lib for express has no typings.

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import swaggerUi from "swagger-ui-express";
+import * as swaggerDocument from "../../openapi.json";
 import listingRouter from "./listing";
 import loginRouter from "./login";
 
@@ -9,5 +11,6 @@ apiV1Router.use("/user", loginRouter);
 apiV1Router.use("/listing", listingRouter);
 
 apiRouter.use("/v1", apiV1Router);
+apiRouter.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 export default apiRouter;
