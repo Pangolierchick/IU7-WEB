09a48814c1a1770d06d120af91de89a59c006a05
rkv <dontgofun@gmail.com>
2023-10-30 01:07:57 +0300

    WIP: add docker clusters, nginx, swagger serving
    TODO:
    
    1. Add mirror in nginx config.
    2. Add mirror replica.
    3. Maybe add swagger config generation.

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:20
+
+WORKDIR /usr/src/app
+
+COPY package.json yarn.lock tsconfig.json openapi.json .env ./
+COPY src ./src
+COPY prisma ./prisma
+
+RUN yarn
+
+EXPOSE 3000
+
+CMD [ "yarn", "start:docker" ]
--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import swaggerUi from "swagger-ui-express";
+import { resolve } from "path";
+import swaggerUi, { SwaggerOptions } from "swagger-ui-express";
 import * as swaggerDocument from "../../openapi.json";
 import listingRouter from "./listing";
 import userRouter from "./user";
@@ -11,6 +12,20 @@ apiV1Router.use("/user", userRouter);
 apiV1Router.use("/listing", listingRouter);
 
 apiRouter.use("/v1", apiV1Router);
-apiRouter.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+apiV1Router.use("/docs", swaggerUi.serve);
+
+const options: SwaggerOptions = {
+  route: {
+    url: "/api/v1/docs",
+  },
+  baseDir: resolve("src"),
+  swaggerDefinition: {
+    host: "localhost",
+    basePath: "/api/v1",
+    produces: ["application/json"],
+  },
+};
+
+apiV1Router.get("/docs", swaggerUi.setup(swaggerDocument, undefined, options));
 
 export default apiRouter;
